61fc9d67bfe3e3806b424c452055124cd1b6bc26
hossein <hosseinenshaei@stu.nit.ac.ir>
2022-08-05 01:14:57 +0430

    Initial commit

---
+++ database/migrations/2022_08_04_200812_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('slug', 60);
+            $table->date('released_date');
+            $table->string('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
